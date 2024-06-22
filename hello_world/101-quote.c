780d971d45a9d8a31342017ea48962585a769ec8
Your Name <you@example.com>
2024-06-22 23:58:05 +0400

    9

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char message[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
-	write(message);
+	char message[] = "and that piece of art is useful" \" - Dora Korpar, 2015-10-19\n", 59;
+	write(STDOUT_FILENO,message, 59);
 	return(1);
 }
