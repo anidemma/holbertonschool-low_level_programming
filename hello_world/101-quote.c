bee08a9dcc2624193cd19e63fc9b463297ff51ed
Your Name <you@example.com>
2024-06-22 23:51:25 +0400

    9

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,13 @@
 include<stdio.h>
-/**
-*/
+include<unistd.h>
 
+/**
+ *  * main - Entry point
+ *   * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char message[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+	write(message);
+	return(1);
+}
