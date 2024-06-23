599458f5eb4ad1cfd2250262a026af67147456d0
Your Name <you@example.com>
2024-06-23 11:54:32 +0400

    4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char abc;
+	
+	e = 'e';
+	q = 'q';
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+		if (abc != e && abc != q)
+		putchar(abc);
+	
+	putchar('\n');
+	return (0);
+}
