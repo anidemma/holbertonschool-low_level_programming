8ff4a20b51c8b59bbdf564e1c9e712ed87acd614
Your Name <you@example.com>
2024-06-23 11:59:18 +0400

    4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,12 @@
 int main(void)
 {
 	char abc;
-	
 	int e = 'e';
 	int q = 'q';
 
 	for (abc = 'a'; abc <= 'z'; abc++)
 		if (abc != e && abc != q)
 		putchar(abc);
-	
 	putchar('\n');
 	return (0);
 }
