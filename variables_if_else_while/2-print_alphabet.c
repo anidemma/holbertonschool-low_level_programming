f4fcbf7496753bba97fa272a17fed19911716ac0
Your Name <you@example.com>
2024-06-23 11:05:10 +0400

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; low <= 'z'; abc++)
+		putchar(abc);
+
+		putchar('\n');
+
+	return (0);
+}
