4f8a4de494d976c7cc50f7e5ce6707d28f23c078
Your Name <you@example.com>
2024-06-23 13:44:40 +0400

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char abc;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	for (abc = 'a'; abc <= 'f'; abc++)
+		putchar(abc);
+	putchar("\n");
+	return (0);
+}
