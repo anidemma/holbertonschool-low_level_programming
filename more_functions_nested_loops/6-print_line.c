813e629cd5b18ec6735e84b52a8b4081f1cc62d4
Your Name <you@example.com>
2024-06-28 13:30:12 +0400

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_line - func
+ * @n: variable
+ */
+void print_line(int n)
+	for (; n > 0 ; n--)
+		if (n > 0)
+			_putchar('_');
+			_putchar('\n');
+		else
+			_putchar('\n');
