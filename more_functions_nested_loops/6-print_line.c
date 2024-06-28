34310453e23d34816924df832d569da75f735574
Your Name <you@example.com>
2024-06-28 14:04:21 +0400

    6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -5,14 +5,12 @@
  */
 void print_line(int n)
 {
-	for (; n > 0 ; n--)
-		if (n > 0)
-		{
+	int i = 0;
+
+	if (n > 0)
+	{
+		for (; i < n; i++)
 			_putchar('_');
-			_putchar('\n');
-		}
-		else
-		{
-			_putchar('\n');
-		}
+	}
+	_putchar('\n');
 }
