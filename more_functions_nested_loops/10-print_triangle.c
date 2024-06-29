e943e699990f6100dfa9ca75b91e70a8f3802d8a
Your Name <you@example.com>
2024-06-29 17:54:14 +0400

    10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - func
+ * @size: variable
+ */
+void print_triangle(int size)
+{
+	int w, l = 0, space;
+
+	if (size > 0)
+	{
+		while (l < size)
+		{
+			space = size - l - 1;
+				while (w < size)
+				{
+					if (space > w)
+						_putchar(' ');
+					else
+						_putchar('#');
+						w++;
+				}
+				l++;
+				w = 0;
+
+				_putchar ('\n');
+		}
+	}
+	else
+	_putchar('\n');
+}
