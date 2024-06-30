805c3566b287a2ce3337b2e13b1c6d5825040635
Your Name <you@example.com>
2024-06-30 18:07:49 +0400

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - function
+ * @s: pointer
+ */
+void print_rev(char *s)
+{
+	int a = 0, b, len;
+
+	while (s[a] != '\0')
+		a++;
+		len = a;
+
+		for (b = len - 1; b >= 0; b--)
+			_putchar(s[b]);
+
+		_putchar('\n');
+}
