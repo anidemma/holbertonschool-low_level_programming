09aed5a8aa17a6d4c1d17f2208ce4b8450bdc5cb
Your Name <you@example.com>
2024-06-30 12:23:51 +0400

    11

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: 0
+ */
+int main(void)
+{
+	long num = 612852475143;
+	int a;
+
+	while (a++ < num / 2)
+	{
+		if (num % a == 0)
+		{
+			num /= 2;
+			continue;
+		}
+		for (a = 3; a < num / 2; a += 2)
+		{
+			if (num % a == 0)
+			num /= a;
+		}
+	}
+	printf("%ld\n", num);
+	return (0);
+}
