645a485b65460628d909ea9a8a82dac63c95155b
Your Name <you@example.com>
2024-06-30 18:41:34 +0400

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - function
+ * @s: pointer
+ */
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char rev;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len--)
+	{
+		aux = s[i];
+		s[i++] = s[len];
+		s[len] = rev;
+	}
+}
