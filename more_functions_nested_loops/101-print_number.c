af61da02f8d7ee5ea97683114bc696b6c85b47d3
Your Name <you@example.com>
2024-06-30 12:53:58 +0400

    11

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - function
+ * @n: variable
+ */
+void print_number(int n)
+{
+	unsigned int a = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		a = -a;
+	}
+	if (a > 9)
+	{
+	print_number(a / 10);
+	}
+	_putchar(a % 10 + '0');
+}
