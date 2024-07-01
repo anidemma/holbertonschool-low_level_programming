fc69ce465cb7f61ebc082505e4a5452bc7643d7d
Your Name <you@example.com>
2024-07-01 16:23:35 +0400

    1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - function
+ * @dest: variable
+ * @src: variable
+ * @n :variable
+ * return: @dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	for (; i < n;  )
+	{
+
+	}
+	return (dest);
+}
