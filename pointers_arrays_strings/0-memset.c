3893c99543e90de349f49afeca03e1c693be8663
Your Name <you@example.com>
2024-07-01 15:10:57 +0400

    0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - function
+ * @s: variable
+ * @b: variable
+ * @n: variable
+ * Return: @s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (n < 0)
+	{
+		s[i] = b;
+		i++;
+		n--;
+	}
+	return (s);
+}
