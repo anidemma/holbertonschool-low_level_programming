3c53c6b6b0f8f1af6f517fcd5c828082e7388669
Your Name <you@example.com>
2024-07-06 15:52:35 +0400

    5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include<stddef.h>
+/**
+ * _strstr - func
+ * @haystack: variable
+ * @needle: variable
+ * Return: haystack or end of the str
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	char *startn = needle, *starth = haystack;
+
+	while (*haystack)
+	{
+		starth = haystack;
+		needle = startn;
+		while (*haystack == *needle)
+		{
+			haystack++;
+			needle++;
+		}
+		if (*needle == '\0')
+			return (haystack);
+			haystack = starth + 1;
+	}
+	return (NULL);
+}
