04d7a370c9c33849b198830c131b602da4cbcdee
Your Name <you@example.com>
2024-07-07 15:20:44 +0400

    1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - function
+ * @s: variable
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_print_rev_recursion(--s);
+	}
+	else
+		_putchar('\n');
+}
