006cf1704bb46df3a9cf8f8fbfb3e0c25ccf1684
Your Name <you@example.com>
2024-07-07 15:06:14 +0400

    0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - function
+ * @s: variable
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+
+	else
+		_putchar('\n');
+}
