ca556e4e4ba677bd42cb34ac3ba31b680e91b342
Your Name <you@example.com>
2024-07-07 15:16:24 +0400

    0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -5,10 +5,10 @@
  */
 void _puts_recursion(char *s)
 {
-	if (s[0] != '\0')
+	if (*s)
 	{
-		_putchar(s[0]);
-		_puts_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(++s);
 	}
 	else
 		_putchar('\n');
