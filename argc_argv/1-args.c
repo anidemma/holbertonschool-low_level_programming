1d78ce5aacec3c0f86d8fd4376022d7836c4b7e2
Your Name <you@example.com>
2024-07-07 18:54:21 +0400

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: variable
+ * @argv: variable
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
