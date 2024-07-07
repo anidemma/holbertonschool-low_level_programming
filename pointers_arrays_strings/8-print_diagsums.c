ce87599f14b39c8d09cde51cec5296757de4072f
Your Name <you@example.com>
2024-07-07 12:13:02 +0400

    7

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - function
+ * @a:matrix
+ * @size: variable
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, dg1 = 0, dg2 = 0;
+
+	for (i = 0; i < size; i++)
+		dg1 += a[i], a += size;
+
+		a -= size;
+
+	for (i = 0; i < size; i++)
+		dg2 += a[i], a -= size;
+
+	printf("%d, %d\n", dg1, dg2);
+}
