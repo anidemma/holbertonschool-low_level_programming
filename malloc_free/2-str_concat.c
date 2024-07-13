9750cea1832f0f9dae1a7a8259c4548a73617b6a
Your Name <you@example.com>
2024-07-13 12:13:49 +0400

    2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include"main.h"
+/**
+ * str_concat - function
+ * @s1: variable
+ * @s2: variable
+ * Return: NULL on failure
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	int a = 0;
+	int b = 0;
+	char *cat;
+
+	if (s1 == NULL)
+		return ("");
+	if (s2 == NULL)
+		return ("");
+	for (i = 0; s1[i] || s2[i]; i++)
+		b++;
+
+	cat = malloc(sizeof(char) * b);
+
+	if (cat == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)
+		cat[a++] = s1[i];
+
+	for (i = 0; s2[i]; i++)
+		cat[a++] = s2[i];
+
+	return (cat);
+}
