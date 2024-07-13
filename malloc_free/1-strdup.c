fe1ce15dc21ce98accb7d174b2f49458e3c9ce3a
Your Name <you@example.com>
2024-07-13 11:11:21 +0400

    1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+ * _strdup - function
+ * @str: variable
+ * Return: pointer to the dub string, NULL otherwise
+ */
+char *_strdup(char *str)
+{
+	int i, b = 0;
+	char *dup;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		b++;
+
+	dup = malloc(sizeof(char) * (b + 1));
+
+	if (dup == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		dup[i] = str[i];
+
+	dup[b] = '\0';
+
+	return (dup);
+}
