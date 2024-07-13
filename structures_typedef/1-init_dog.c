4dffea84ab3eaf435bbd799640c03e6b4263fcea
Your Name <you@example.com>
2024-07-13 20:56:42 +0400

    1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - function
+ * @d: struct
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}
