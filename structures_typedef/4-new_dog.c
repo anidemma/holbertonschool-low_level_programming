2c5c1d638c2daf607f5f99d830ba3ca07bb4e712
Your Name <you@example.com>
2024-07-14 16:28:01 +0400

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -29,20 +29,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(doge);
 		return (NULL);
 	}
+	for (k = 0; k <= i; k++)
+		n[k] = name[k];
 	o = malloc(sizeof(char) * j + 1);
+
+	doge->name = name;
+	doge->age = age;
+
 	if (o == NULL)
 	{
 		free(n);
 		free(doge);
 		return (NULL);
 	}
-	for (k = 0; k =< i; k++)
-		n[k] = name[k];
-	for (k = 0; k =< j; k++)
+	for (k = 0; k <= j; k++)
 		o[k] = owner[k];
 
-	doge->name = name;
-	doge->age = age;
 	doge->owner = o;
 
 	return (doge);
