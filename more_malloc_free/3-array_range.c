808b71b2057cdd44fc37112977ba0d9be9d2eb84
Your Name <you@example.com>
2024-07-14 19:27:44 +0400

    3

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * array_range - function
+ * @min: min
+ * @max: max
+ * Return: a
+ */
+int *array_range(int min, int max)
+{
+	int i = 0, *a;
+
+	if (min > max)
+		return (NULL);
+
+	a = malloc(sizeof(int) * (max - min + 1));
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = min; i <= max; i++)
+		a[i - min] = i;
+
+	return (a);
+}
