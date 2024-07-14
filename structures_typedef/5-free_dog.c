2b6742ee77c0834edbdf6abc54f4a80d2e2e3517
Your Name <you@example.com>
2024-07-14 17:48:13 +0400

    5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,16 +1,15 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * free_dog - function
- * @d: variable
- */
+ *  * free_dog - func
+ *   * @d: var
+ *    */
 void free_dog(dog_t *d)
 {
 	if (d != NULL)
 	{
-
-	free(d->name);
-	free(d->owner);
-	free(d);
+		free((*d).name);
+		free((*d).owner);
+		free(d);
 	}
 }
