0f4bea9f6852131bbae4daf760824fa75944bbc1
Your Name <you@example.com>
2024-07-16 09:55:15 +0400

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
-#include "main.h"
+#include "function_pointers.h"
 /**
  * print_name - function
  * @name: variable
- * @f:fuction
+ * @f: function
  */
 void print_name(char *name, void (*f)(char *))
 {
