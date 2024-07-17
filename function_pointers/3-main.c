a666d0950fb3720e710fe5ca51be4abd04b7047e
Your Name <you@example.com>
2024-07-17 18:22:01 +0400

    3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -10,7 +10,6 @@
 int main(int argc, char *argv[])
 {
 	int a, b;
-	char x;
 	int (*o)(int, int);
 
 	if (argc != 4)
@@ -25,9 +24,8 @@ int main(int argc, char *argv[])
 	}
 
 	a = atoi(argv[1]), b = atoi(argv[3]), o = get_op_func(argv[2]);
-	x = (char)o;
 
-	if (o == '/' || o == '%')
+	if (*o == '/' || *o == '%')
 	{
 		printf("Error\n");
 		exit(100);
