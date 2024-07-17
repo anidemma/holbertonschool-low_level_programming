abcb8240153b6329a8c48e34fefd0f1f8f36bbc3
Your Name <you@example.com>
2024-07-17 16:29:54 +0400

    3

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -4,12 +4,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stddef.h>
-
+/**
+ * struct op - func
+ * @op:var
+ * @f: var
+ */
 typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-}
+} op_t;
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
