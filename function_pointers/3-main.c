bad0ba2eed6e21299614d46466c10682b9c64a87
Your Name <you@example.com>
2024-07-17 15:46:43 +0400

    3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "calc.c"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function
+ * @argc: variable
+ * @argv: variable
+ * Return: NULL
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*o)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	o = get_op_func(argv[2]);
+	if (0 == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d", o(a, b));
+
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,46 @@
+#include "calc.h"
+
+/**
+ * op_add - function
+ * @a:var1
+ * @b:var2
+ * Return: a + b
+*/
+int op_add(int a, int b)
+	return (a + b);
+
+/**
+ * op_sub - function
+ * @a:var1
+ * @b:var2
+ * Return: a - b
+*/
+int op_sub(int a, int b)
+	return (a - b);
+
+/**
+ * op_mul - function
+ * @a:var1
+ * @b:var2
+ * Return: a * b
+*/
+int op_mul(int a, int b)
+	return (a * b);
+
+/**
+ * op_div - function
+ * @a:var1
+ * @b:var2
+ * Return: a / b
+*/
+int op_div(int a, int b)
+	return (a / b);
+
+/**
+ * op_mod - function
+ * @a:var1
+ * @b:var2
+ * Return: a % b
+*/
+int op_mod(int a, int b)
+	return (a % b);
