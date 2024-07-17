403ed9e07905fb679e82280262e0ef00ff342a43
Your Name <you@example.com>
2024-07-17 18:18:53 +0400

    3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 
 	a = atoi(argv[1]), b = atoi(argv[3]), o = get_op_func(argv[2]);
 
-	if ((char)o == '/' || o == '%')
+	if (argv[2] == '/' || o == '%')
 	{
 		printf("Error\n");
 		exit(100);
