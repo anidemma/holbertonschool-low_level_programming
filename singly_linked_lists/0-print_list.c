1fbe9b73248b990213811b67dd1a09c5f730ee55
AliyevaMadina <mdn.alyv@gmail.com>
2024-07-29 14:55:05 +0400

    0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,11 +8,16 @@ size_t print_list(const list_t *h)
 	size_t i = 0;
 
 	if (h->str == NULL)
+	{
 		printf("[0] (nil)\n");
+		h = h->next;
+		i++;
+	}
 	while (h)
 	{
 		printf("[%d] %s\n", h->len, h->str);
-		h = h->next, i++;
+		h = h->next;
+		i++;
 	}
 	return (i);
 }
