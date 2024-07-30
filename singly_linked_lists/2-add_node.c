c225fb06940b8d4da39b5f8803bf87f6deda7524
AliyevaMadina <mdn.alyv@gmail.com>
2024-07-30 17:09:20 +0400

    2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * add_node - function
+ * @head: variable
+ * @str: variable
+ * Return: adress of new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int i = 0, len = 0;
+	char *new_node;
+	list_t *new;
+
+	while (*(str + i))
+		len++, i++;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+
+	i = 0;
+	new = *head;
+	new_node = malloc(sizeof(char) * (len + 1));
+
+	if (new_node == NULL)
+		return (NULL);
+	while (*(str + i))
+		*(new_node + i) = *(str + i), i++;
+	*(new_node + i) = '\0';
+	new = malloc(sizeof(size_t));
+	if (new == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new->str = new_node;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
