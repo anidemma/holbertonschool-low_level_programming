faaa11143badbc73476f8d7ed23a34f965776283
AliyevaMadina <mdn.alyv@gmail.com>
2024-08-03 20:47:40 +0400

    7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - function
+ * @h: double pointer to the head of linked list
+ * @idx: index
+ * @n: data
+ * Return: address of the new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tmp = *h, *new_node;
+	unsigned int i = 1;
+
+	new_node = NULL;
+
+	if (idx != 0)
+	{
+		while (tmp)
+		{
+			if (i == idx)
+			{
+				if (tmp->next == NULL)
+					new_node = add_dnodeint_end(h, n);
+				else
+				{
+					new_node = malloc(sizeof(dlistint_t));
+					if (new_node)
+					{
+						new_node->n = n;
+						new_node->prev = tmp;
+						new_node->next = tmp->next;
+						tmp->next->prev = new_node;
+						tmp->next = new_node;
+					}
+				}
+				break;
+			}
+			tmp = tmp->next;
+			i++;
+		}
+	}
+	else
+		new_node = add_dnodeint(h, n);
+
+	return (new_node);
+}
