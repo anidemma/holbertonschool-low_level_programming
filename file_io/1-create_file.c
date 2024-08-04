38967a8af0eb1f660772a900399c7ae509885f50
AliyevaMadina <mdn.alyv@gmail.com>
2024-08-04 16:21:00 +0400

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,7 +3,7 @@
  * create_file - function
  * @filename: filename
  * @text_content: content
- * Return: 1 , -1 otherwise
+ * Return: 1, -1 otherwise
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -11,20 +11,25 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
 	if (fd == -1)
 		return (-1);
-	if (text_content)
+	if (!text_content)
 	{
-		while (text_content[len])
-			len++;
-		w = write(fd, text_content, len);
-		if (w == -1 || w != len)
-		{
-			close(fd);
-			return (-1);
-		}
+		close(fd);
+		return (1);
 	}
+	else
+		while (*(text_content + i))
+		i++;
+
+	w = write(fd, text_content, i);
+
+	if (w != i || w == -1)
+		return (-1);
+
 	close(fd);
 	return (1);
 }
