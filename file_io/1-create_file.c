2fd5ad48a2d56ed02181b85ee158576ac958af66
AliyevaMadina <mdn.alyv@gmail.com>
2024-08-04 16:11:49 +0400

    1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * create_file - function
+ * @filename: filename
+ * @text_content: content
+ * Return: 1 , -1 otherwise
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int i = 0, fd, w;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	else
+		while (*(text_content + i))
+				i++;
+	w = write(fd, text_content, i);
+
+	if (w != i || w == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
