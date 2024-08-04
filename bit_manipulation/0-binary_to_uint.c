6d073b7eef2f73a7e590f9298cb3776113b0f078
AliyevaMadina <mdn.alyv@gmail.com>
2024-08-04 13:57:37 +0400

    0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * binary_to_uint - function
+ * @b: variable
+ * Return: converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0, len = 0, res = 0;
+
+	if (!b)
+		return (0);
+	while (*(b + len))
+		len++;
+	for (; i < len; i++)
+	{
+		if (*(b + i) > '1' || *(b + i) < '0')
+			return (0);
+		res = (res << 1) + (*(b + i) - '0');
+	}
+	return (res);
+}
